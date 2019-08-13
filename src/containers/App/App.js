58c5f4b5cc61586b2b6715f1bdb03d10aa30129c
lofix <maciekk.milewski@gmail.com>
2019-08-13 13:24:50 +0200

    add more paths and remove unused css file

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 import Header from '../Header/Header';
+import FruitStorage from '../FruitStorage/FruitStorage';
+
 import Home from '../../components/Home/Home';
-import './App.css';
+import modules from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={modules.App}>
         <Header />
         <Layout>
           <Switch>
-            <Route exact path="/" component={Home}/>
+            <Route path="/packaging" component={Home} />
+            <Route path="/shipments" component={Home} />
+            <Route path="/sorting" component={Home} />
+            <Route path="/fruit-storage" component={FruitStorage} />
+            <Route exact path="/" component={Home} />
           </Switch>
         </Layout>
       </div>
