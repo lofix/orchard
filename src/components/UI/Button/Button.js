f54fe8f94ef66347e652d48ab6d6a3d020b30fab
lofix <maciekk.milewski@gmail.com>
2019-08-13 13:22:54 +0200

    install more dependencies

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import modules from './Button.module.css';
 
 const button = (props) => {
-  return (
-    <button className={modules.Button}>{props.copy}</button>
-  )
+  const button = <button 
+                    className={[modules.Button, modules[props.btnType], modules[props.colorSet]].join( ' ')}>
+                    {props.withIcon ? <i className={props.iconClasses}></i> : null}
+                    {props.copy}
+                  </button>;
+  return (props.isLink && props.path ? <Link to={props.path}>{button}</Link> : button)
 }
 
 export default button;
