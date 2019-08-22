dcbcfcda35ae54ce6b8b5edb0cbcd9c4a0938421
lofix <maciekk.milewski@gmail.com>
2019-08-22 11:37:59 +0200

    create Spinner component

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import modules from './Spinner.module.css';
+
+const spinner = () => {
+  return (
+    <div className={modules.Loader}></div>
+  )
+}
+
+export default spinner;
